09d9ad9032644ff3e7508c0a2db5b9f066fee7cb
zhanghaojie <1455123087@qq.com>
2025-12-15 14:29:36 +0800

    feat: add beforeLoad hook to Route for managing opened project IDs

--- src/routes/projects/$id.tsx
+++ src/routes/projects/$id.tsx
@@ -1,8 +1,12 @@
 import { ProjectDetail } from "@/features/project-detail";
+import { addOpenedId } from "@/stores/opened-projects";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/projects/$id")({
   component: RouteComponent,
+  beforeLoad: (ctx) => {
+    addOpenedId(ctx.params.id);
+  },
 });
 
 function RouteComponent() {
