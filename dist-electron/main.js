ad103f52139afbe3ec23b14aea0123d2417204d7
zhanghaojie <1455123087@qq.com>
2025-12-16 10:48:04 +0800

    refactor: streamline main process and preload script for improved readability and maintainability

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,70 +1,58 @@
-import { ipcMain, dialog, shell, nativeImage, app, BrowserWindow } from "electron";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-import fs from "node:fs";
-const __dirname$1 = path.dirname(fileURLToPath(import.meta.url));
-process.env.APP_ROOT = path.join(__dirname$1, "..");
-const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
-const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
-const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
-let win;
-function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+import { ipcMain as r, dialog as R, shell as g, nativeImage as h, app as l, BrowserWindow as c } from "electron";
+import { fileURLToPath as E } from "node:url";
+import e from "node:path";
+import _ from "node:fs";
+const p = e.dirname(E(import.meta.url));
+process.env.APP_ROOT = e.join(p, "..");
+const i = process.env.VITE_DEV_SERVER_URL, v = e.join(process.env.APP_ROOT, "dist-electron"), A = e.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = i ? e.join(process.env.APP_ROOT, "public") : A;
+let o;
+function d() {
+  o = new c({
+    icon: e.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: path.join(__dirname$1, "preload.mjs"),
-      webSecurity: false
+      preload: e.join(p, "preload.mjs"),
+      webSecurity: !1
     }
-  });
-  win.webContents.on("did-finish-load", () => {
-    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  });
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL);
-  } else {
-    win.loadFile(path.join(RENDERER_DIST, "index.html"));
-  }
+  }), o.webContents.on("did-finish-load", () => {
+    o == null || o.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  }), i ? o.loadURL(i) : o.loadFile(e.join(A, "index.html"));
 }
-ipcMain.handle("app:add-material-dialog", async () => {
-  const { canceled, filePaths } = await dialog.showOpenDialog({
+r.handle("app:add-material-dialog", async () => {
+  const { canceled: t, filePaths: n } = await R.showOpenDialog({
     properties: ["openFile", "multiSelections"]
   });
-  if (canceled || filePaths.length === 0) {
+  if (t || n.length === 0)
     return [];
-  }
-  const files = [];
-  for (const filePath of filePaths) {
-    const stats = fs.statSync(filePath);
-    const name = path.basename(filePath);
-    files.push({
-      name,
-      path: filePath,
-      size: stats.size
+  const s = [];
+  for (const a of n) {
+    const m = _.statSync(a), f = e.basename(a);
+    s.push({
+      name: f,
+      path: a,
+      size: m.size
     });
   }
-  return files;
+  return s;
 });
-ipcMain.handle("app:open-file-location", (_, filePath) => {
-  shell.showItemInFolder(filePath);
+r.handle("app:open-file-location", (t, n) => {
+  g.showItemInFolder(n);
 });
-ipcMain.on("app:start-drag", (event, filePath) => {
-  const icon = nativeImage.createFromDataURL("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==");
-  event.sender.startDrag({
-    file: filePath,
-    icon
+r.on("app:start-drag", (t, n) => {
+  const s = h.createFromDataURL("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==");
+  t.sender.startDrag({
+    file: n,
+    icon: s
   });
 });
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+l.on("activate", () => {
+  c.getAllWindows().length === 0 && d();
 });
-app.whenReady().then(() => {
-  createWindow();
+l.whenReady().then(() => {
+  d();
 });
 export {
-  MAIN_DIST,
-  RENDERER_DIST,
-  VITE_DEV_SERVER_URL
+  v as MAIN_DIST,
+  A as RENDERER_DIST,
+  i as VITE_DEV_SERVER_URL
 };
