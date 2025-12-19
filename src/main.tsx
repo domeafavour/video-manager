f0787cfe2f78e837987842faaddfe04f24c9c566
zhanghaojie <1455123087@qq.com>
2025-12-19 17:25:01 +0800

    feat: integrate memory history into router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createMemoryHistory,
+  createRouter,
+} from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { queryClient } from "./queryClient";
 import { routeTree } from "./routeTree.gen";
 
+const history = createMemoryHistory();
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
